3774cbc966844af8cac09983817430ddd6852dd3
henadalGH <hrnnnadal@gmail.com>
2025-09-17 11:55:52 -0300

    Creacion de spring boot

---
+++ WMotorPro/src/main/java/com/Taller/WMotorPro/WMotorPro/WMotorProApplication.java
@@ -0,0 +1,13 @@
+package com.Taller.WMotorPro.WMotorPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WMotorProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WMotorProApplication.class, args);
+	}
+
+}
---
+++ WMotorPro/src/test/java/com/Taller/WMotorPro/WMotorPro/WMotorProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Taller.WMotorPro.WMotorPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WMotorProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
