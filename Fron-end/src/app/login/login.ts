a781127f283af7bf98a726ad208e721ffe49551c
henadalGH <hrnnnadal@gmail.com>
2025-11-27 17:06:24 -0300

    Agregondo el agregar un vehicilo en el from

--- Fron-end/src/app/login/login.ts
+++ Fron-end/src/app/login/login.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../Auth/auth-service';
+
 import { LoginServide } from './login-servicio';
 
 @Component({
