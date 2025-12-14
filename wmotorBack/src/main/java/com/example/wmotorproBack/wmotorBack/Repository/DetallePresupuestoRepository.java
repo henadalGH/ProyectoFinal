92e04d00a1e2e784acf8f66b2edbe5e02c8c0cf5
henadalGH <hrnnnadal@gmail.com>
2025-12-14 16:25:11 -0300

    Arreglado algunos errores

--- wmotorBack/src/main/java/com/example/wmotorproBack/wmotorBack/Repository/DetallePresupuestoRepository.java
+++ wmotorBack/src/main/java/com/example/wmotorproBack/wmotorBack/Repository/DetallePresupuestoRepository.java
@@ -3,7 +3,9 @@ package com.example.wmotorproBack.wmotorBack.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.wmotorproBack.wmotorBack.Modelo.Entity.DetallePresupuestoEntity;
+
 @Repository
-public interface DetallePresupuestoRepository extends JpaRepository<DetallePresupuestoRepository, Long>{
+public interface DetallePresupuestoRepository extends JpaRepository<DetallePresupuestoEntity, Long>{
 
 }
--- wmotorBack/src/main/java/com/example/wmotorproBack/wmotorBack/Repository/ProveedorRepository.java
+++ wmotorBack/src/main/java/com/example/wmotorproBack/wmotorBack/Repository/ProveedorRepository.java
@@ -3,7 +3,9 @@ package com.example.wmotorproBack.wmotorBack.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.wmotorproBack.wmotorBack.Modelo.Entity.ProveedorEntity;
+
 @Repository
-public interface ProveedorRepository extends JpaRepository<ProveedorRepository, Long> {
+public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Long> {
 
 }
